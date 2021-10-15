07993ee50144f97706258eb3de85b71babe9b3b0
Silas Santos da Silva <silvasilas66@gmail.com>
2021-10-15 11:14:32 -0300

    Making browser route to weekday

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WeekdayController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/getMessage', [WeekdayController::class, 'index']);
